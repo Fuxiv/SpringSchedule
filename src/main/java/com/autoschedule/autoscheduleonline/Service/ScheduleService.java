87899023c95462ef82a11068345e167fe7801cd4
Jakub Szymański <simple.jacob@icloud.com>
2023-05-16 07:07:41 +0200

    fix minuses, better seconds randomization.

--- src/main/java/com/autoschedule/autoscheduleonline/Service/ScheduleService.java
+++ src/main/java/com/autoschedule/autoscheduleonline/Service/ScheduleService.java
@@ -25,51 +25,35 @@ public class ScheduleService {
         return String.format("%" + (-L) + "s", input).replace(' ', ch);
     }
 
-
-    private final Random random = new Random();
-
-
-    private String randomSecondsTime(String stopTime, int rSeconds) {
+    private String randomSecondsTime1(String stopTime, int rSeconds) {
         String[] stopNameSplit = stopTime.split(":");
         short stopMinutes = Short.parseShort(stopNameSplit[1]);
+        short stopHours = Short.parseShort(stopNameSplit[0]);
         String stopHoursString = stopNameSplit[0];
-        if (rSeconds < 10 && rSeconds >= 0) {
-            return stopTime + "0" + rSeconds;
-        }
-        if (rSeconds < 0) {
+        if (rSeconds <= -10) {
             stopMinutes--;
             if (stopMinutes < 0) {
-                short stopHours = Short.parseShort(stopNameSplit[0]);
-                stopMinutes = 59;
                 stopHours--;
-                if (stopHours < 0) {
-                    stopHours = 23;
-                    if (rSeconds > -10) {
-                        return stopHours + ":" + stopMinutes + ":" + "0" + Math.abs(rSeconds);
-                    }
-                    return stopHours + ":" + stopMinutes + ":" + Math.abs(rSeconds);
-                }
-                if (stopHours < 10) {
-                    if (rSeconds > -10) {
-                        return "0" + stopHours + ":" + stopMinutes + ":" + "0" + Math.abs(rSeconds);
-                    }
-                    return "0" + stopHours + ":" + stopMinutes + ":" + Math.abs(rSeconds);
-                }
-                if (rSeconds > -10) {
-                    return stopHours + ":" + stopMinutes + ":" + "0" + Math.abs(rSeconds);
-                }
-                return stopHours + ":" + stopMinutes + ":" + Math.abs(rSeconds);
-            }
-            if (rSeconds > -10 && stopMinutes < 10) {
-                return stopHoursString + ":" + "0" + stopMinutes + ":" + Math.abs(rSeconds);
-            }
-            if (rSeconds > -10) {
-                return stopHoursString + ":" + stopMinutes + ":" + "0" + Math.abs(rSeconds);
+                return stopHours + ":" + "59" + ":" + Math.abs(rSeconds);
             }
             if (stopMinutes < 10) {
                 return stopHoursString + ":" + "0" + stopMinutes + ":" + Math.abs(rSeconds);
             }
-            return stopNameSplit[0] + ":" + stopMinutes + ":" + Math.abs(rSeconds);
+            return stopHoursString + ":" + stopMinutes + ":" + Math.abs(rSeconds);
+        }
+        if (rSeconds < 0) {
+            stopMinutes--;
+            if (stopMinutes < 0) {
+                stopHours--;
+                return stopHours + ":" + "59" + ":" + "0" + Math.abs(rSeconds);
+            }
+            if (stopMinutes < 10) {
+                return stopHoursString + ":" + "0" + stopMinutes + ":" + "0" + Math.abs(rSeconds);
+            }
+            return stopHoursString + ":" + stopMinutes + ":" + "0" + Math.abs(rSeconds);
+        }
+        if (rSeconds < 10) {
+            return stopTime + "0" + rSeconds;
         }
         return stopTime + rSeconds;
     }
@@ -87,7 +71,7 @@ public class ScheduleService {
         }
         return "  00:00:" + rSeconds;
     }
-
+    private final Random random = new Random();
     public String writeFirstLine(int trip_index_id, int line_number_id) {
         List<BusStop> busStopList = busStopService.getStopByLineAndTrip(trip_index_id, line_number_id);
         String stopName = busStopList.get(0).getName();
@@ -95,8 +79,8 @@ public class ScheduleService {
         if (stopName.length() > 30) {
             stopName = stopName.substring(0, 30);
         }
-        int rSeconds = random.nextInt(-55, 55);
-        return "  " + rightPadding(stopName, ' ', 32) + " ".repeat(30) + stopTime + "00" + "  " + randomSecondsTime(stopTime, rSeconds) + secondsDifference(stopTime, rSeconds) + "  ok";
+        int rSeconds = random.nextInt(-5, 55);
+        return "  " + rightPadding(stopName, ' ', 32) + " ".repeat(30) + stopTime + "00" + "  " + randomSecondsTime1(stopTime, rSeconds) + secondsDifference(stopTime, rSeconds) + "  ok";
     }
 
     public String writeLastLine(int trip_index_id, int line_number_id) {
@@ -107,7 +91,7 @@ public class ScheduleService {
             stopName = stopName.substring(0, 30);
         }
         int rSeconds = random.nextInt(-55, 55);
-        return "  " + rightPadding(stopName, ' ', 32) + stopTime + "00" + "  " + randomSecondsTime(stopTime, rSeconds) + secondsDifference(stopTime, rSeconds) + " ".repeat(32) + "ok";
+        return "  " + rightPadding(stopName, ' ', 32) + stopTime + "00" + "  " + randomSecondsTime1(stopTime, rSeconds) + secondsDifference(stopTime, rSeconds) + " ".repeat(32) + "ok";
     }
 
     public String writeDefaultLine(int trip_index_id, int line_number_id, short stopNumber) {
@@ -124,16 +108,19 @@ public class ScheduleService {
         if (stopName.length() > 30) {
             stopName = stopName.substring(0, 30);
         }
-        int arrivalTimeSeconds = random.nextInt(-45, 30);
+//        int arrivalTimeSeconds = random.ints(-45, 30).findFirst().getAsInt();
+//        int departureTimeSeconds = random.ints(-10, 55).findFirst().getAsInt();
+        int arrivalTimeSeconds = (int) Math.floor(Math.random() * (30 + -45) + -45);
         int departureTimeSeconds = random.nextInt(-10, 55);
-        System.out.println("departureTimeSeconds:" + departureTimeSeconds);
-        System.out.println("arrivalTimeSeconds:" + arrivalTimeSeconds);
-        while (departureTimeSeconds < arrivalTimeSeconds) {
+                //to jest git, po prostu minusy trzeba ogarnac do porzadku
+//        System.out.println("arrivalTimeSeconds:" + arrivalTimeSeconds);
+//        System.out.println("departureTimeSeconds:" + departureTimeSeconds);
+        while (Math.abs(departureTimeSeconds) < Math.abs(arrivalTimeSeconds)) {
             departureTimeSeconds = random.nextInt(arrivalTimeSeconds, 55);
-            System.out.println(arrivalTimeSeconds + "   " + departureTimeSeconds);
+//            System.out.println(arrivalTimeSeconds + "   " + departureTimeSeconds);
         }
-        String arrivalTime = randomSecondsTime(stopTime, arrivalTimeSeconds);
-        String departureTime = randomSecondsTime(stopTime, departureTimeSeconds);
+        String arrivalTime = randomSecondsTime1(stopTime, arrivalTimeSeconds);
+        String departureTime = randomSecondsTime1(stopTime, departureTimeSeconds);
         return "  " + rightPadding(stopName, ' ', 32) + stopTime + "00" + "  " + arrivalTime + secondsDifference(stopTime, arrivalTimeSeconds) + "  " + stopTime + "00" + "  " + departureTime + secondsDifference(stopTime, departureTimeSeconds) + "  " + "ok";
     }
 
